e0109984e1cb5eae6d471cd5dd903cf699600423
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-07-11 13:11:18 -0700

    updated main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use fxread::initialize_reader;
 
 fn main() {
-    let path = "sequences.fq";
+    let path = "example/sequences.fq";
     let reader = initialize_reader(path).expect("invalid path");
     reader
         .into_iter()
