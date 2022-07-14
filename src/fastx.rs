696702882af476261427ea6f7d8d7269fff5a1af
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-07-14 12:31:32 -0700

    added documentation for fastx trait

--- src/fastx.rs
+++ src/fastx.rs
@@ -1,6 +1,9 @@
 use super::Record;
 use anyhow::Result;
 
+/// A trait for Fasta and Fastq readers
 pub trait FastxRead : Iterator {
+
+    /// Returns the next fastx [`Record`] in the iterator.
     fn next_record(&mut self) -> Result<Option<Record>>;
 }
